91ea7d6de16ce75019e058865e77a29975a436c6
kranthi <kranthiteja222@gmail.com>
2025-11-21 23:50:29 +0530

    how to store output of command

---
+++ 03.StoreOutputOfCommand.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo " let try to store output of the command"
+
+PWD=$(pwd)
+
+echo "present working directory is $PWD"
+
+myname=$(whoami)
+
+echo "I am $myname"
+
+date=$(date)
+
+echo "today is $date"
+
+
