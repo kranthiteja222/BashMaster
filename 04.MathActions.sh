2537dd61dc12b76219018bdb5e5c1470499aa8b7
kranthi <kranthiteja222@gmail.com>
2025-11-22 22:32:12 +0530

    Arithmatic Operation

---
+++ 04.MathActions.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "we are going to learn how to perform math operation"
+
+echo $((2+3))
+
+echo $((2*3))
+
+echo $((5/3))
+
+echo $((5%2))
+
+echo $((5-2))
+
+echo $(($RANDOM % 10))
+
+
+
